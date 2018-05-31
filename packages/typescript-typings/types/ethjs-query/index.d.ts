c59fd35779c481568ad2032e9790542c8ae2ae72
Mariano A. Cortesi <mariano@flowics.com>
2018-05-31 16:59:25 -0300

    chore: add more typings for ethjs-query

--- packages/typescript-typings/types/ethjs-query/index.d.ts
+++ packages/typescript-typings/types/ethjs-query/index.d.ts
@@ -2,10 +2,25 @@ declare module 'ethjs-query' {
   import { BN } from 'bn.js';
 
   type Address = string;
-
+  export interface Event {
+    transactionHash: string;
+  }
   export interface TransactionReceipt {
     contractAddress: string;
     gasUsed: BN;
+  }
+  export interface TransactionInfo {
+    hash: string;
+    nonce: BN;
+    blockHash: string;
+    blockNumber: BN;
+    transactionIndex: BN;
+    from: string;
+    to: string;
+    value: BN;
+    gas: BN;
+    gasPrice: BN;
+    input: string;
   }
 
   class Eth {
@@ -15,6 +30,9 @@ declare module 'ethjs-query' {
     getBalance(account: Address): Promise<BN>;
 
     getTransactionReceipt(txHash: string): Promise<TransactionReceipt>;
+    getTransactionByHash(txHash: string): Promise<TransactionInfo>;
+    blockNumber(): Promise<BN>;
+    sign(account: Address, str: string): Promise<string>;
   }
 
   export default Eth;
