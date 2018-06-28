421da33df345bd2f21a31d3e63371e0bffb8b466
Mariano A. Cortesi <mcortesi@gmail.com>
2018-06-28 12:47:44 -0300

    fix: dont use general css, scope to dexdex-root

--- packages/widget/src/index.tsx
+++ packages/widget/src/index.tsx
@@ -3,4 +3,8 @@ import * as ReactDOM from 'react-dom';
 import Widget from './components/Widget';
 import { appConfig } from './config';
 
+if (window.parent !== window) {
+  document.body.classList.add('content-root');
+}
+
 ReactDOM.render(<Widget widgetId={appConfig().widgetId} />, document.getElementById('dexdex-root'));
