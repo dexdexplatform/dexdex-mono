888b309878c0610e6c5e06bd810ebbc4bcb30651
Sebastian Zelonka <sebastianzelonka@gmail.com>
2018-08-01 18:33:40 -0300

    Added styles on NoWalletModal

--- packages/widget/src/components/NoWalletModal.tsx
+++ packages/widget/src/components/NoWalletModal.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { Modal } from './Modal';
 import { isEmbed } from '../config';
 
+const classes = require('./NoWalletModal.css');
+
 export interface NoWalletModalProps {
   closeModal: () => void;
 }
@@ -18,22 +20,33 @@ const TrustURL =
   'https://links.trustwalletapp.com/a/key_live_lfvIpVeI9TFWxPCqwU8rZnogFqhnzs4D' +
   `?&event=openURL&url=${encodeURIComponent(getCurrentUrl())}`;
 
+const logoTrust = require('./images/logoTrust.png');
+const logoToshi = require('./images/logoToshi.png');
+const logoStatus = require('./images/logoStatus.png');
+
 class NoWalletModal extends React.PureComponent<NoWalletModalProps> {
   render() {
     return (
       <Modal onClose={this.props.closeModal}>
-        <div>No tenes wallet loco! Media pila</div>
-        <div>
-          Usa{' '}
-          <a target="_top" href={TrustURL}>
-            TRUST!
-          </a>
+        <div className={classes.header}>
+          <h1>Wallet App Required</h1>
         </div>
-        <div>
-          Y sino te gusta, usa{' '}
-          <a target="_top" href="https://get.status.im/browse/easytrade.io">
-            Status
-          </a>
+        <div className={classes.content}>
+          <p>To use EasyTrade you need to install a wallet app. We suggest the followings:</p>
+
+          <div className={classes.links}>
+            <a target="_top" href={TrustURL}>
+              <img src={logoTrust} />
+            </a>
+
+            <a href="http://onelink.to/8yv352">
+              <img src={logoToshi} />
+            </a>
+
+            <a target="_top" href="https://get.status.im/browse/easytrade.io">
+              <img src={logoStatus} />
+            </a>
+          </div>
         </div>
       </Modal>
     );
